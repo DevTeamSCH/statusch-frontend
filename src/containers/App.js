24058f7e8bcb6f34d92fc50d918655ba49d3e3cf
Tamás Szabó <tamas@szaboo.com>
2017-12-21 23:07:48 +0100

    addedd footer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { NavLink, Link, Route, Switch, Redirect } from 'react-router-dom'
-import { Menu, Image, Container } from 'semantic-ui-react'
+import { Menu, Image, Container, Segment } from 'semantic-ui-react'
 
 import { getLaundry } from '../actions'
 import { Page, Printer, Study, NotFound } from '../components'
@@ -12,6 +12,12 @@ const menuStyle = {
   backgroundColor: '#fff',
   border: '1px solid #ddd',
   boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
+}
+
+const styles = {
+  page: {
+    height: '100%',
+  },
 }
 
 class App extends Component {
@@ -25,26 +31,31 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Menu fixed='top' style={menuStyle} borderless>
-          <Container>
-            <Menu.Item header>
-              <Image size='mini' src='favicon.ico' />
-            </Menu.Item>
-            <Menu.Item as={Link} to='/laundry-rooms'>MosógépSCH</Menu.Item>
-            <Menu.Item as={NavLink} to='/study-rooms'>TanulóSCH</Menu.Item>
-            <Menu.Item as={NavLink} to='/printer'>PrinterSCH</Menu.Item>
-          </Container>
-        </Menu>
-        <Page>
-          <Switch>
-            <Redirect exact from='/' to='/laundry-rooms' />
-            <Route path='/laundry-rooms' component={LaundryContainer} />
-            <Route path='/study-rooms' component={Study} />
-            <Route path='/printer' component={Printer} />
-            <Route component={NotFound} />
-          </Switch>
-        </Page>
+      <div style={{ height: 'calc(100% - 47px)' }}>
+        <div style={styles.page}>
+          <Menu fixed='top' style={menuStyle} borderless>
+            <Container>
+              <Menu.Item header>
+                <Image size='mini' src='favicon.ico' />
+              </Menu.Item>
+              <Menu.Item as={Link} to='/laundry-rooms'>MosógépSCH</Menu.Item>
+              <Menu.Item as={NavLink} to='/study-rooms'>TanulóSCH</Menu.Item>
+              <Menu.Item as={NavLink} to='/printer'>PrinterSCH</Menu.Item>
+            </Container>
+          </Menu>
+          <Page>
+            <Switch>
+              <Redirect exact from='/' to='/laundry-rooms' />
+              <Route path='/laundry-rooms' component={LaundryContainer} />
+              <Route path='/study-rooms' component={Study} />
+              <Route path='/printer' component={Printer} />
+              <Route component={NotFound} />
+            </Switch>
+          </Page>
+        </div>
+        <Segment inverted vertical textAlign='center'>
+          Created by DevTeam
+        </Segment>
       </div>
     )
   }
